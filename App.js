56ea54057b99aad36b7ac381f07f3ba76b64d018
nathalieperra22 <nathalie.perra22@trinityschoolnyc.org>
2023-11-08 21:40:21 -0800

    Animations upon opening app
    Added a fade animation for the logo

--- App.js
+++ App.js
@@ -1,29 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Image,View } from 'react-native';
-//import { Button } from 'react-native';
-//import TrackPlayer from 'react-native-track-player';
+import { StyleSheet, Text, Image, View, Animated } from 'react-native';
+import React, {useRef, useEffect} from 'react';
 
+//Logo Animation 
+const Fade = props => {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 2500,
+        useNativeDriver: true,
+      }), //.start(); //,
+    Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 2500,
+        useNativeDriver: true,
+      })
+    ]).start();
+  }, [fadeAnim]);
+
+    return (
+      <Animated.View 
+        style={{
+          ...props.style,
+          opacity: fadeAnim, 
+        }}>
+        {props.children}
+      </Animated.View>
+    );
+};
 
 //streamURL will hold the Radio Stream link and we can reference this later...
 const streamUrl = 'https://securestreams4.autopo.st:1643/';
 
 export default function App() {
-  
   return (
-
-
-    
-    <View style={styles.container}>
-      <View style={styles.topHalf}>
+    <View style={styles.container} >
+      <Fade 
+        style={styles.topHalf}>
         <Image source={require('./assets/Logo.png')} style={styles.logo} />
-      </View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Ucla Radio Dev Baddddiees hard at work </Text>
-      <StatusBar style="auto" />
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Ucla Radio Dev Baddddiees hard at work </Text>
+      </Fade>
     </View>
   );
 }
-//comment
 
 const styles = StyleSheet.create({
   container: {
